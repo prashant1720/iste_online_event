42f72a51253166c4efbf2ab7274b24d560d4a48a
Prashant sharma <prashantssharma21@gmail.com>
2020-04-10 21:57:12 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/iste_online_event/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.iste_online_event
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/DepartmentPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class DepartmentPage extends StatelessWidget {
+ // final wheelNumber;
+  // DepartmentPage(this.wheelNumber);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink,
+
+
+
+
+    );
+  }
+}
